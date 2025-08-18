84dcb14cd232dbdba6d634c5c77f53cc322fd66b
Wiktor Bajor <wiktorbajor1@gmail.com>
2025-08-18 21:30:31 +0200

    Remove tests body

--- examples/test/service_provider_tests.cpp
+++ examples/test/service_provider_tests.cpp
@@ -28,86 +28,8 @@ protected:
 
 TEST_F(ServiceProviderTest, WhenServiceRequestReceived_ThenStateIsUpdated)
 {
-  auto node = std::make_shared<test_composition::ServiceProvider>(opts);
-
-  /// Retrieve the service created by the Node
-  auto service = rtest::findService<std_srvs::srv::SetBool>(node, "/test_service");
-
-  // Check that the Node actually created the Service
-  ASSERT_TRUE(service);
-
-  // Initial state should be false
-  ASSERT_FALSE(node->getState());
-
-  /// Set up test request
-  auto request_header = std::make_shared<rmw_request_id_t>();
-  request_header->sequence_number = 123L;
-  auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
-  request->data = true;
-
-  // Set up expected response
-  std_srvs::srv::SetBool::Response expected_response;
-  expected_response.success = true;
-  expected_response.message = "State updated successfully";
-
-  // Set up expectation that service will handle request and set proper response
-  EXPECT_CALL(*service, send_response(*request_header, expected_response));
-
-  // Simulate service call
-  service->handle_request(request_header, request);
-
-  // The state should be updated to true
-  EXPECT_TRUE(node->getState());
 }
 
 TEST_F(ServiceProviderTest, WhenServiceIsLocked_ThenStateIsNotUpdated)
 {
-  auto node = std::make_shared<test_composition::ServiceProvider>(opts);
-
-  /// Retrieve the service created by the Node
-  auto service = rtest::findService<std_srvs::srv::SetBool>(node, "/test_service");
-
-  // Check that the Node actually created the Service
-  ASSERT_TRUE(service);
-
-  // Initial state should be false
-  ASSERT_FALSE(node->getState());
-
-  node->lockService();
-
-  /// Set up test request
-  auto request_header = std::make_shared<rmw_request_id_t>();
-  request_header->sequence_number = 123L;
-  auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
-  request->data = true;
-
-  // Set up expected response
-  std_srvs::srv::SetBool::Response expected_response;
-  expected_response.success = false;
-  expected_response.message = "Service is locked, cannot update state";
-
-  // Set up expectation that service will handle request and set proper response
-  EXPECT_CALL(*service, send_response(*request_header, expected_response));
-
-  // Simulate service call
-  service->handle_request(request_header, request);
-
-  // The state should not be updated because the service was locked
-  EXPECT_FALSE(node->getState());
-
-  // Unlock the service
-  node->unlockService();
-
-  // Set up expected response for unlocking
-  expected_response.success = true;
-  expected_response.message = "State updated successfully";
-
-  // Set up expectation that service will handle request and set proper response
-  EXPECT_CALL(*service, send_response(*request_header, expected_response));
-
-  // Simulate service call again
-  service->handle_request(request_header, request);
-
-  // The state should be updated to true
-  EXPECT_TRUE(node->getState());
 }
